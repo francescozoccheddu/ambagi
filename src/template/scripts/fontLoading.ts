20060cff3d3b3c4bf3379075e57e468e7a7ea253
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2024-01-13 15:55:39 +0100

    Add fonts ready listener

---
+++ src/template/scripts/fontLoading.ts
@@ -0,0 +1,31 @@
+let loaded = false;
+const listeners: Set<() => void> = new Set();
+
+function onFontsReady(): void {
+  if (!loaded) {
+    loaded = true;
+    listeners.forEach(l => l());
+    listeners.clear();
+  }
+}
+
+function setup(): void {
+  setTimeout(onFontsReady, 1000);
+  try {
+    void document.fonts.ready.then(onFontsReady);
+  } catch { /* empty */ }
+  try {
+    document.fonts.addEventListener('loadingdone', onFontsReady);
+  } catch { /* empty */ }
+}
+
+export function addFontsReadyListener(listener: () => void): void {
+  if (loaded) {
+    listener();
+  }
+  else {
+    listeners.add(listener);
+  }
+}
+
+setup();
--- src/template/scripts/main.ts
+++ src/template/scripts/main.ts
@@ -1,7 +1,8 @@
+import { addFontsReadyListener } from 'ambagi/fontLoading';
 import { setLoaded } from 'ambagi/loading';
 import { setupTooltips } from 'ambagi/tooltips';
 import { setupVideos } from 'ambagi/videos';
 
 setupVideos();
 setupTooltips();
-setTimeout(setLoaded, 1000);
+addFontsReadyListener(setLoaded);
