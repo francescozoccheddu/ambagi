06aa78bdcf1f0f64a36b0ba1a0b22f9990e8b2c6
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2024-01-13 15:52:09 +0100

    Add publish script

--- src/template/scripts/main.ts
+++ src/template/scripts/main.ts
@@ -1,5 +1,7 @@
 import { setLoaded } from 'ambagi/loading';
+import { setupTooltips } from 'ambagi/tooltips';
 import { setupVideos } from 'ambagi/videos';
 
 setupVideos();
+setupTooltips();
 setTimeout(setLoaded, 1000);
---
+++ src/template/scripts/tooltips.ts
@@ -0,0 +1,3 @@
+export function setupTooltips(): void {
+
+}
