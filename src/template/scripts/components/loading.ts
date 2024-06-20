62ae0d5ccd716e0965b88d59390a5e3d9aec5c0f
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2024-06-20 20:03:31 +0200

    Add page reveal animation

--- src/template/scripts/components/loading.ts
+++ src/template/scripts/components/loading.ts
@@ -4,13 +4,17 @@ export function setLoaded(): void {
   if (!loaded) {
     loaded = true;
     const main = document.getElementById('main')!;
-    main.addEventListener('animationstart', () => {
-      document.body.classList.remove('faded-in');
-      document.body.classList.add('fading-in');
+    main.addEventListener('animationstart', e => {
+      if (e.target == main) {
+        document.body.classList.remove('faded-in');
+        document.body.classList.add('fading-in');
+      }
     });
-    main.addEventListener('animationend', () => {
-      document.body.classList.add('faded-in');
-      document.body.classList.remove('fading-in');
+    main.addEventListener('animationend', e => {
+      if (e.target == main) {
+        document.body.classList.add('faded-in');
+        document.body.classList.remove('fading-in');
+      }
     });
     document.body.classList.add('loaded');
   }
